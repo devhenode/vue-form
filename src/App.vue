cb04b9baa49379a2a56416465824e11a32d85fbd
devhenode <devodehenry.2000@gmail.com>
2023-12-12 16:06:20 +0100

    first under development

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <SignupForm />
+</template>
+
+<script>
+import SignupForm from './components/SignupForm.vue'
+
+export default {
+  name: 'App',
+  components: { SignupForm }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  text-align: center;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
